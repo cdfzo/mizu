c07013cb340275867db63597ff4b388666600a58
cdfzo <cdfzo@pm.me>
2024-03-07 21:27:16 +0100

    Add extension.ts

---
+++ src/extension.ts
@@ -0,0 +1,5 @@
+/** This function is called when the extension is activated. */
+export const activate = () => {}
+
+/** This function is called when the extension is deactivated. */
+export const deactivate = () => {}
